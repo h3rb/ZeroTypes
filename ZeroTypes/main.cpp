35edeee270728ca44b2223daee9011f18d185de6
h3rb <hgilliland@gmail.com>
2017-09-22 08:22:40 -0400

    Update main.cpp

--- ZeroTypes/main.cpp
+++ ZeroTypes/main.cpp
@@ -1,7 +1,8 @@
 /*
 
- ZeroTypes Library
- Open source license: BSD 4-clause
+ ZeroTypes Library 
+ Open source license: BSD 
+ (See accompanying License.txt for details)
 
  Author: H. Elwood Gilliland III
  Copyright (c) 2007-2017
