d9aa8553bc867e016dd1d63c348d0c73366f3a4f
h3rb <hgilliland@gmail.com>
2017-09-22 08:25:23 -0400

    Update string_functions.cpp

--- ZeroTypes/string_functions.cpp
+++ ZeroTypes/string_functions.cpp
@@ -1,7 +1,8 @@
 ﻿/*
 
  ZeroTypes Library
- Open source license: BSD 4-clause
+ Open source license: BSD 
+ (See accompanying License.txt for details)
 
  Author: H. Elwood Gilliland III
  Copyright (c) 2007-2017
